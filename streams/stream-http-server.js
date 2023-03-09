80a4fbb9470b7144da02a70f9490220e53e88947
brunoper3s <brunoperes7@hotmail.com>
2023-03-09 10:42:28 -0300

    Streams Nodejs - Req.body

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -9,10 +9,22 @@ class InverseNumberSteam extends Transform {
   }
 }
 
-const server = http.createServer((req, res) => {
-  return req
+const server = http.createServer(async (req, res) => {
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString()
+
+  console.log(`${fullStreamContent}`);
+
+  return res.end(fullStreamContent)
+
+  /* return req
     .pipe(new InverseNumberSteam())
-    .pipe(res)
+    .pipe(res) */
 })
 
 server.listen(3334)
